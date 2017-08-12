2ea2723c91a6207219dbb91725eaa6843b979c30
Geoff Wark <me@geoffmail.com>
2017-08-12 11:57:14 -0400

    Finish Day 1 Q&A

--- test/test.cpp
+++ test/test.cpp
@@ -9,6 +9,10 @@ void foo(void);
 
 void foo(void) // Function signature or prototype... or definition
 {
+	// 'A' means ASCII (or ANSI?)  -->  8-bit
+	// 'W' means wide (Unicode)    --> 16-bit
+
+	// This is basically bypassing the macro so we can print an ASCII string.
 	OutputDebugStringA(
 		"This is the first thing we have actually printed.\n");
 }
