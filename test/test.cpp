54224419982761486a67774c3555cb5e015e8a76
Geoff Wark <me@geoffmail.com>
2017-08-20 16:26:17 -0400

    Finish Day 3

--- test/test.cpp
+++ test/test.cpp
@@ -5,11 +5,19 @@
 multi-line comment
 */
 
+void bar(void)
+{
+	int Barvariable;
+	
+	Barvariable = 100;
+}
+
 // Function declaration
 void foo(void);
 
 void foo(void) // Function signature or prototype... or definition
 {
+	/*
 	// 'A' means ASCII (or ANSI?)  -->  8-bit
 	// 'W' means wide (Unicode)    --> 16-bit
 
@@ -25,6 +33,13 @@ void foo(void) // Function signature or prototype... or definition
 	// This gets translated by Windows so you don't need the \r in most cases.
 
 	printf("\nThis is a thing.\n");
+	*/
+
+	int Foovariable;
+	
+	Foovariable = 200;
+
+	bar();
 }
 
 
@@ -34,6 +49,7 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
+	/*
 	char SmallS; // 8 bits  -  256 different values (-128 to 127)
 	signed char SmallU; // 8 bits unsigned  -  256 diff values (0 to 255)
 
@@ -42,18 +58,24 @@ int CALLBACK WinMain(
 	
 	int LargeS; // 32 bits -  4 billion
 	int unsigned LargeU;
-
-	int Integer;
-
+	*/
+	//int Integer;
+	
 	char unsigned Test;
+	char unsigned *TestPointer;
+
+	TestPointer = &Test;
+
 	Test = 255;
 	Test = Test + 1;
 
 	// Funny story about = meaning 'assignment' not 'equals'
+	/*
 	Integer = 5;
 	Integer = 5 + 2;
 
 	Integer = Integer + 5;
+	*/
 
 	foo();
 }
